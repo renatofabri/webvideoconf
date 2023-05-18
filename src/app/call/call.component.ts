4d28566263b66446abea77713912613006850afb
Renato Fabri <renato.sfabri@gmail.com>
2023-05-18 00:12:50 -0300

    Fix issue with remoteVideoTrack, fix ScreenShare, refactor code

--- src/app/call/call.component.ts
+++ src/app/call/call.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AgoraService } from '../services/agora.service';
+import AgoraRTC from 'agora-rtc-sdk-ng';
 
+interface ChannelParameters {
+  localAudioTrack: any;
+  localVideoTrack: any;
+  remoteAudioTrack: any;
+  remoteVideoTrack: any;
+  screenTrack: any;
+  remoteUid: any;
+}
 @Component({
   selector: 'app-call',
   templateUrl: './call.component.html',
@@ -8,48 +16,110 @@ import { AgoraService } from '../services/agora.service';
 })
 export class CallComponent implements OnInit {
 
-  @ViewChild("localVideo", { static: true }) localVideo: any;
-  @ViewChild("remoteVideo", { static: true }) remoteVideo: any;
-  @ViewChild("screenVideo", { static: true }) screenVideo: any;
+  channelParameters: ChannelParameters = {
+    localAudioTrack: null,
+    localVideoTrack: null,
+    remoteAudioTrack: null,
+    remoteVideoTrack: null,
+    screenTrack: null,
+    remoteUid: null,
+  };
 
-  apiMessages = '';
+  @ViewChild("localVideoContainer", { static: true }) localVideoContainer: any;
+  @ViewChild("remoteVideoContainer", { static: true }) remoteVideoContainer: any;
+  @ViewChild("screenContainer", { static: true }) screenContainer: any;
+
+  message = '';
   uid = Math.floor(Math.random() * 1000);
-  channel = '127de39f-7241-49f4-86cc-2bb76255bc64';
 
   isSharingEnabled = false;
   isMuteVideo = false;
 
-  constructor(private agoraService: AgoraService) {}
+  agoraEngine: any;
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.agoraService.startBasicCall();
+    this.message = '';
 
-    this.agoraService.message.subscribe((message: string) => {
-      this.apiMessages = message;
+    this.agoraEngine = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+
+    this.agoraEngine.on('user-published', async (user: any, mediaType: any) => {
+      console.log("someone joined");
+      await this.agoraEngine.subscribe(user, mediaType);
+      console.log('subscribe success');
+
+      if (mediaType == 'audio') {
+        this.channelParameters.remoteUid = user.uid;
+        this.channelParameters.remoteAudioTrack = user.audioTrack;
+        this.channelParameters.remoteAudioTrack.play();
+        this.message = 'Remote user connected: ' + user.uid;
+      }
+
+      if (mediaType == 'video') {
+        this.channelParameters.remoteUid = user.uid;
+        this.channelParameters.remoteVideoTrack = user.videoTrack;
+        this.channelParameters.remoteVideoTrack.play('remoteVideoContainer');
+        this.message = 'Remote user connected: ' + user.uid;
+      }
+
+      this.agoraEngine.on('user-unpublished', (user: any) => {
+        console.log(user.uid + 'has left the channel');
+        this.message = 'Remote user has left the channel';
+      });
     });
   }
 
-  showMessage(text: string) {
-    this.apiMessages = text;
+  async join() {
+    console.log('Joining channel: ' + this.channel);
+    await this.agoraEngine.join(
+      this.appId,
+      this.channel,
+      this.rtcToken,
+      this.uid);
+    console.log('joined');
+
+    this.channelParameters.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+
+    this.channelParameters.remoteVideoTrack?.play('remoteVideoContainer');
+
+    await this.agoraEngine.publish([
+      this.channelParameters.localAudioTrack,
+    ]);
   }
-  token() {
-    console.log(this.agoraService.generateRtcToken(this.uid, "channelTest"));
+  async joinVideo() {
+    console.log('Joining video: ' + this.channel);
+    this.channelParameters.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
+    this.channelParameters.localVideoTrack.play('localVideoContainer');
+
+    await this.agoraEngine.publish([
+      this.channelParameters.localVideoTrack,
+    ]);
   }
 
-  join() {
-    this.agoraService.join(this.uid, this.channel);
-  }
-
-  leave() {
-    this.agoraService.leave();
+  async leave() {
+    this.channelParameters.localAudioTrack.close();
+    this.channelParameters.localVideoTrack.close();
+    this.channelParameters.remoteVideoTrack.close();
+    this.channelParameters.screenTrack.close();
+    await this.agoraEngine.leave();
+    console.log('You left the channel');
   }
 
   async inItScreen() {
-    this.agoraService.initScreen(this.isSharingEnabled);
-  }
-
-  muteVideo() {
-
+    this.isSharingEnabled = !this.isSharingEnabled;
+    if (this.isSharingEnabled) {
+      this.channelParameters.screenTrack = await AgoraRTC.createScreenVideoTrack({ encoderConfig: '720p_3' });
+      this.channelParameters.screenTrack.play('screenContainer');
+      this.agoraEngine.unpublish([this.channelParameters.localVideoTrack]);
+      this.agoraEngine.publish([this.channelParameters.screenTrack]);
+      await this.channelParameters.localVideoTrack.replaceTrack(this.channelParameters.screenTrack, true);
+    } else {
+      this.agoraEngine.unpublish([this.channelParameters.screenTrack]);
+      this.channelParameters.screenTrack.close();
+      this.agoraEngine.publish([this.channelParameters.localVideoTrack]);
+      await this.channelParameters.localVideoTrack.replaceTrack(this.channelParameters.localVideoTrack, true);
+    }
   }
 
   localAudioVolume(val: any) {
